5a636904a6e28b3cbb9c594f88c5286b5d1b7b9b
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-05 16:35:03 -0700

    Test: FileReader readers spec text file

--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -9,12 +9,14 @@ RSpec.describe FileReader do
   ARGV[0] = './spec/fixtures/message_spec.txt'
 
   it 'exists' do
+    file = FileReader.new
     expect(file).to be_a(FileReader)
   end
 
   it 'reads a file' do
-    if ARGV[0] == 'message.txt'
-      expect(file.read_english).to eq('hello')
+    if ARGV[0] == './spec/fixtures/message_spec.txt'
+      file = File.read('./spec/fixtures/message_spec.txt')
+      expect(file).to eq("hello spec\n")
     end
   end
 
