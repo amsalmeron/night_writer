4863395b5f81e3bb3bfb28ce3042ce2da5c345b8
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-05 18:31:07 -0700

    Test: can translate two letters into array braille

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -6,13 +6,25 @@ SimpleCov.start
 RSpec.describe Translator do
 
   interpretor = Translator.new('./spec/fixtures/testing.txt')
+  interpretor_2 = Translator.new('./spec/fixtures/testing_2.txt')
 
   it 'exists' do
     expect(interpretor).to be_a(Translator)
   end
 
-  it 'can translate a letter' do
-    expect(interpretor.english_to_braille_array).to eq([['0', '.'], ['.', '.'], ['.', '.']])
+  it 'can translate letter "a" ' do
+    expect(interpretor.english_to_braille_array).to eq([['0', '.'],
+                                                        ['.', '.'],
+                                                        ['.', '.']])
+  end
+
+  it 'can translate letter "ab" ' do
+    expect(interpretor_2.english_to_braille_array).to eq([['0', '.'],
+                                                        ['.', '.'],
+                                                        ['.', '.'],
+                                                        ['0', '.'],
+                                                        ['0', '.'],
+                                                        ['.', '.']])
   end
 
 end
