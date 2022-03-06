f92e88c9a65c7abf36e6eca57e95e139ed77c0a2
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-05 17:59:33 -0700

    Test: #english_to_braille

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -5,10 +5,14 @@ SimpleCov.start
 
 RSpec.describe Translator do
 
-  interpretor = Translator.new('fasho')
+  interpretor = Translator.new('testing.txt')
 
   it 'exists' do
     expect(interpretor).to be_a(Translator)
+  end
+
+  it 'can translate a letter' do
+    expect(interpretor.english_to_braille).to eq([['0', '.'], ['.', '.'], ['.', '.']])
   end
 
 end
