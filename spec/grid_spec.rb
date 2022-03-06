6d793663b322980ae9660d776303bb68f6fd39bb
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-05 17:41:12 -0700

    Feat: Grid class to create Braille character grid

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -5,11 +5,11 @@ SimpleCov.start
 RSpec.describe Grid do
 
   context 'Iteration 2: Building a Braille character grid' do
-    grid = Grid.new(['.', '.'], ['.', '.'], ['.', '.'])
-  
+    grid = Grid.new
+
     it 'create a 3x2 grid' do
-      expect(output1).to be_a(Grid)
-      expect { output1.render }.to output(
+      expect(grid).to be_a(Grid)
+      expect { grid.render }.to output(
         <<~EXPECTED
           ..
           ..
