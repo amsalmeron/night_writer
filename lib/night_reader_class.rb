7df95cc14c6e2bd1e630554082df126183b2ad49
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-07 12:04:29 -0700

    Refector: delete unneccessary classes Gird and FileWrite as well as add NightReader class seperate from runner

---
+++ lib/night_reader_class.rb
@@ -0,0 +1,22 @@
+require './lib/braille_translator'
+
+class NightReader
+  def initialize
+    @input = ARGV[0]
+    @output = ARGV[1]
+    @translator = BrailleTranslator.new(@input)
+  end
+
+  def create_new_english_file
+    @output = File.new(ARGV[1], "w")
+    @output.puts(@translator.braille_to_english_array)
+    @output.close
+  end
+
+  def display
+    if @input != nil
+    puts "Created '#{File.basename(@output)}'" \
+    " containing #{(File.read(@input).length - 1) / 6} characters"
+    end
+  end
+end
