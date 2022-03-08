f6244758bb5c15dddb69bd04a85cfc105e175708
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-03-07 18:43:26 -0700

    Feat: #braille_to_english_array can translate multiple arrays

--- lib/braille_translator.rb
+++ lib/braille_translator.rb
@@ -10,20 +10,42 @@ class BrailleTranslator
   end
 
   def braille_to_english_array
-    i = 0
     words = []
     characters = (File.read(@message)).split("\n")
-    characters.each_slice(3) do |top,mid,bottom|
-      (top.length / 2).times do
-        letter = []
-        letter << top[i..i+1]
-        letter << mid[i..i+1]
-        letter << bottom[i..i+1]
-        words << braille_dictionary[letter]
-        i += 2
+    each_line = characters.each_slice(3).to_a
+    add_space = each_line.count
+    each_line.each do |line|
+      line.each_slice(3) do |top,mid,bottom|
+        i = 0
+        (top.length / 2).times do
+          letter = []
+          letter << top[i..i+1]
+          letter << mid[i..i+1]
+          letter << bottom[i..i+1]
+          words << braille_dictionary[letter]
+          i += 2
+        end
       end
+      words << " " if add_space > 1
+      add_space -= 1
     end
     words.join
+
+    # i = 0
+    # words = []
+    # characters = (File.read(@message)).split("\n")
+    # require "pry"; binding.pry
+    # characters.each_slice(3) do |top,mid,bottom|
+    #   (top.length / 2).times do
+    #     letter = []
+    #     letter << top[i..i+1]
+    #     letter << mid[i..i+1]
+    #     letter << bottom[i..i+1]
+    #     words << braille_dictionary[letter]
+    #     i += 2
+    #   end
+    # end
+    # words.join
   end
 
   def line_split
@@ -38,6 +60,17 @@ class BrailleTranslator
       a += 3
     end
     english_rows
+  end
+
+  def character_group
+    characters = []
+    a = 0
+    line_group.each do |line|
+      line.each do |char|
+        characters << char[a..a+1]
+      end
+    end
+    a += 2
   end
 
   # def character_split
